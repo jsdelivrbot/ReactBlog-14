295f08f34dc49838bfabd66b10725ce5a7eeba6b
nida <nida.bouzid@gmail.com>
2017-08-21 21:07:45 +0100

    massaging the response

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';//import promise
+
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);//apply it to middleware call
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import { FETCH_POSTS } from '../actions'; //nothing to mention since we import from index.js
+import _ from lodash;
+
+export default function (state = {}, action) {//default state to empty objectß
+    switch (action.type) {
+        case FETCH_POSTS:
+            console.log(action.payload.data);//[post1, post2]
+            return _.mapKeys(action.payload.data, 'id');//massage data , { id1: post1, id2: post2}
+        default:
+            return state;
+    }
+}
