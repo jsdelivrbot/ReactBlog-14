54058bf618b04957c4a94799396f1560d3787927
nida <nida.bouzid@gmail.com>
2017-09-10 22:01:37 +0800

    move to loopback

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,8 +6,9 @@ const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=thisIsMyK3y';
 
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-
+    //const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    const request = axios.get('http://localhost:3000/api/posts');
+    
     return {
         type: FETCH_POSTS,
         payload: request //automatically resolved, reponse here
