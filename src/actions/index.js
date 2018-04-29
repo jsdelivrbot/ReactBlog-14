706c3b2f6913f4360e9b801478fb5447482ef167
Nida Bouzid <nbo@activeviam.com>
2018-04-29 18:14:34 +0800

    minor modif

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,17 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=thisIsMyK3y';
+// const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+// const API_KEY = '?key=thisIsMyK3y';
 
 export function fetchPosts() {
     //const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
-    const request = axios.get('http://localhost:3000/api/posts');
+    const request = axios.get('http://localhost:3000/posts');
     
     return {
         type: FETCH_POSTS,
-        payload: request //automatically resolved, reponse here
+        //automatically resolved, reponse here,
+        // add request result to the payload of the action we re returning
+        payload: request 
     };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,7 +1,7 @@
 import { FETCH_POSTS } from '../actions'; //nothing to mention since we import from index.js
 import _ from 'lodash';
 
-export default function (state = {}, action) {//default state to empty objectß
+export default function (state = {}, action) {//default state to empty object
     switch (action.type) {
         case FETCH_POSTS:
             console.log(action.payload.data);//[post1, post2]
