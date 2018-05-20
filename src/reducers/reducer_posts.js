f0ca85b6b08d571709c0c8a2b5f3b0a384158aac
Nida Bouzid <nbo@activeviam.com>
2018-05-20 21:33:42 +0800

    enhance the reducer

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,6 +3,8 @@ import _ from 'lodash';
 
 export default function (state = {}, action) {//default state to empty object
     switch (action.type) {
+        case DELETE_POST:
+            return _.omit(state, action.payload);//returns new state object with the post id (action.payload) removed from it
         case FETCH_POST:
             //es5
             //const post = action.payload.data;
