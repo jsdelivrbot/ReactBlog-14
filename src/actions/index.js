663803b8f8b07800160ec4cfb50281c3a11b052b
Nida Bouzid <nbo@activeviam.com>
2018-05-20 20:58:23 +0800

    add the delete

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
-
+export const DELETE_POST  = 'delete_post';
 
 // const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 // const API_KEY = '?key=thisIsMyK3y';
@@ -34,9 +34,19 @@ export function createPost(values, callback) {
 
 export function fetchPost(id) {
     const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
-  
+
     return {
-      type: FETCH_POST,
-      payload: request
+        type: FETCH_POST,
+        payload: request
     };
-  }
+}
+
+export function deletePost(id, callback) {
+    const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: DELETE_POST,
+        payload: id
+    }
+}
